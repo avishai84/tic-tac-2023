a4859b821b64b3836de12cbdbe052c58de95811f
Avishai Taashur <avishai@avishai84.com>
2023-10-06 16:18:17 -0700

    add hover state next player

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,6 +4,25 @@ import {makeBoard, checkForRowWinner, checkForColumnWinner, checkForDiagonal, sh
 import useScores from "../hooks/useScores";
 
 const DivGame = styled.div ``;
+const DivGameWithIcon = styled.div<{ nextPlayer: string, isCellOccupied:boolean }>`
+@keyframes fadein {
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 0.5;
+    }
+}
+
+&.nextPlayerHover:before {
+    position: absolute;
+    content: ${props => (props.isCellOccupied ? '""' : `"${props.nextPlayer}"`)};
+    font-size: 7.5vw;
+    opacity: 0;
+    animation: ${props => (props.isCellOccupied ? 'none' : 'fadein 250ms forwards')};
+}
+`;
+
 const Span = styled.span `
 font-size: 7.5vw; text-shadow: 0px 3px 4px ##2e7b02;`;
 const Div = styled.div `
@@ -16,6 +35,7 @@ justify-content: center;
 font-size: calc(10px + 2vmin);
 color: white;
 `;
+
 
 type SizeProps = {
     size: string;
@@ -41,7 +61,11 @@ const Board = ({size, playersIcon}:SizeProps):JSX.Element => {
 const handleClick = (rowIndex:number, cellIndex:number) => {
    
     const newBoard = board && [...board];
-   
+//  check if current cell is empty
+    if (newBoard && newBoard[rowIndex][cellIndex] !== "") {
+        return;
+    }
+
     if(newBoard){
         let currentPlayer = newBoard[rowIndex][cellIndex];
             currentPlayer = player === playerIconsState[0] ? playerIconsState[1] : playerIconsState[0];
@@ -63,17 +87,24 @@ const handleClick = (rowIndex:number, cellIndex:number) => {
     }
   };
   
-
+  const nextPlayer = player === playerIconsState[1] ? playerIconsState[0] : playerIconsState[1];
 
   const playerBoard = useMemo(() => (
     board && board.map((row, rowIndex):JSX.Element => {
         return(<DivGame key={rowIndex} style={{display:"flex", justifyContent:"center", gap:"2px"}}>
             {row.map((cell:string, cellIndex:number) => {
-                return(<DivGame 
+                return(<DivGameWithIcon 
+                    nextPlayer={nextPlayer}
+                    isCellOccupied={cell !== ""}
+                    // on hover toggle a class name of playerHover
+                    onMouseEnter={(e) => { e.currentTarget.classList.add("nextPlayerHover"); }
+                    }
+                    onMouseLeave={(e) => { e.currentTarget.classList.remove("nextPlayerHover"); }
+                    }
                     onClick={() => handleClick(rowIndex, cellIndex)}
                     style={{display: "flex", gap:"0",
                 justifyContent: "center", boxShadow:"0px 0px 0px 1px white inset",
-                alignItems: "center",outline: "2px solid white", height:"10vw", width:"10vw"}} key={cellIndex}><Span>{cell}</Span></DivGame>)
+                alignItems: "center",outline: "2px solid white", height:"10vw", width:"10vw"}} key={cellIndex}><Span>{cell}</Span></DivGameWithIcon>)
             })}
         </DivGame>)
     })
@@ -83,7 +114,7 @@ useEffect(() => {
     setBoard(makeBoard(size));
     playersIcon && setPlayerIcons(playersIcon);
     playersIcon && setPlayer(playersIcon[0]);
-}, [size, playersIcon]);
+}, [size, playersIcon, scores]);
 
 useEffect(() => {
     if (winner) {
@@ -112,10 +143,14 @@ const keepScores = (<table>
 </table>);
 
     return(<>
-        {winner && <DivGame><Div><Span>{winner} </Span><h1>win</h1> <button onClick={() => {setWinner(null);setBoard(makeBoard(size));}}>Play Again</button></Div></DivGame>}
+        {winner && <DivGame><Div><Span>{winner} </Span><h1>win</h1>
+        <button onClick={() => {setWinner(null);setBoard(makeBoard(size));
+        }
+        }>Play Again</button></Div></DivGame>}
     <Div>
-        <div>{keepScores}<span><button onClick={() => resetScores()}>Reset Scores</button></span></div>
-        {playerBoard}</Div></>);
+        <div>{keepScores}<span><button onClick={() => {resetScores()}}>Reset Scores</button></span></div>
+        <div>{playerBoard}</div>
+        </Div></>);
 };
 
 export default Board;
