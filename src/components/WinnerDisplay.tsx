ca9b685dff13942b3a81d34a11fffc9931e66577
Avishai Taashur <avishai@avishai84.com>
2023-10-08 12:59:33 -0700

    add lucid icons

---
+++ src/components/WinnerDisplay.tsx
@@ -0,0 +1,18 @@
+import {RefreshCw} from "lucide-react"
+
+type WinnerDisplayProps = {
+    winner: string | null;
+    resetGame: () => void;
+};
+
+const WinnerDisplay = ({ winner, resetGame }: WinnerDisplayProps) => {
+    return (
+        <div>
+            {winner && <h2>{winner} Won!</h2>}
+            <button onClick={resetGame}>
+            <RefreshCw />New Game
+            </button>
+        </div>
+    );
+};
+export default WinnerDisplay;
